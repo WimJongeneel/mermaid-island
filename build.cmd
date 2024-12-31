636dde3b4623c3567d1645065f92afe43d909f1b
WimJongeneel <wim.jongeneel1@gmail.com>
2024-12-31 11:14:37 +0100

    Fix tsx reference in tsm file

--- build.cmd
+++ build.cmd
@@ -4,11 +4,15 @@ set cp_folder=".\[CP] FlowerGarden"
 set ts_out_folder="C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley\Mods\[TrainStation] FlowerGarden"
 set ts_folder=".\[TrainStation] FlowerGarden"
 
+set map_src="C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley\Content (unpacked)\Maps\flower-garden.tmx"
+set map_target=".\[CP] FlowerGarden\assets"
+
 dotnet build
+
+copy /y %map_src% %map_target%
 
 if exist %cp_out_folder% rmdir /s /q %cp_out_folder%
 robocopy %cp_folder% %cp_out_folder% /e
-
 
 if exist %ts_out_folder% rmdir /s /q %ts_out_folder%
 robocopy %ts_folder% %ts_out_folder% /e
