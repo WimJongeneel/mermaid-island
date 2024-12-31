ea98bd004135c657055611a1dadf523a883c4cb6
WimJongeneel <wim.jongeneel1@gmail.com>
2024-12-31 15:53:10 +0100

    Fix water animations and brigde collisions

---
+++ reset_map_source.cmd
@@ -0,0 +1,4 @@
+set map_src="C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley\Content (unpacked)\Maps\flower-garden.tmx"
+set map_target=".\[CP] FlowerGarden\assets"
+
+copy /y %map_target% %map_src% 
